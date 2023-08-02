68a9bff9b3ce7831193c9495a13b1b860709109f
TATHIANY KANASHIRO <tathymarquesani@gmail.com>
2023-08-02 10:45:52 -0300

    Add files via upload
    Novos exercícios sobre For

---
+++ exercicio7.java
@@ -0,0 +1,14 @@
+public class exercicio7 {
+    public static void main(String args[]){
+
+        for(int i=0; i<5; i++){
+            System.out.println("Valor de i: "+ i);
+        }
+
+        System.out.println("\n");
+
+        for(int j=4; j>=0;j--){
+            System.out.println("Valor de i: "+ j);
+        }
+    }
+}
